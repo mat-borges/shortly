d67be5905a5a1d239950271c333c7eea05e3f767
Mateus Borges <borgesm.work@gmail.com>
2022-12-19 23:49:34 -0300

    feat: visits count update when open shor url

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -38,11 +38,12 @@ export async function getUrlById(req, res) {
 export async function openShortUrl(req, res) {
   const { shortUrl } = req.params;
   try {
-    const urlExist = await connection.query(`SELECT url FROM urls WHERE "shortUrl"=$1`, [shortUrl]);
+    const urlExist = await connection.query(`SELECT id, url, "visitCount" FROM urls WHERE "shortUrl"=$1`, [shortUrl]);
     if (!urlExist.rows[0]) {
       return res.sendStatus(404);
     } else {
-      const url = urlExist.rows[0].url;
+      const { url, id, visitCount } = urlExist.rows[0];
+      await connection.query(`UPDATE urls SET "visitCount"=$1 WHERE id=$2`, [visitCount + 1, id]);
       return res.redirect(url);
     }
   } catch (err) {
@@ -52,5 +53,12 @@ export async function openShortUrl(req, res) {
 }
 
 export async function deleteShortUrl(req, res) {
-  res.sendStatus(501);
+  const id = parseInt(req.locals.id);
+  try {
+    await connection.query(`SELECT * FROM url WHERE id=$1`, [id]);
+    res.sendStatus(501);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
