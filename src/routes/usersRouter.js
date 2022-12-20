e2a7a4ff0b0d1ac914a1676008cc373e5da08446
Mateus Borges <borgesm.work@gmail.com>
2022-12-20 00:58:15 -0300

    feat: get logged in user info (route /user/me)

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { getUser } from '../controllers/userController.js';
+import { verifyAuthorization } from '../middlewares/verifyAuthorizationMiddleware.js';
 
 const router = Router();
 
-router.get(`/users/me`, getUser);
+router.get(`/users/me`, verifyAuthorization, getUser);
 
 export default router;
