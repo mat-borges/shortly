160943263efababb56c928a5ff8ca9c27aee9452
Mateus Borges <borgesm.work@gmail.com>
2022-12-20 01:12:20 -0300

    feat: get ranking (top 10 "most visited users")

--- src/routes/rankingRouter.js
+++ src/routes/rankingRouter.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { getRankings } from '../controllers/rankingController.js';
 
 const router = Router();
 
-router.get(`/ranking`);
+router.get(`/ranking`, getRankings);
 
 export default router;
